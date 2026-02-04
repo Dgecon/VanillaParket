008a7836f82dbb3b5526b51a7ad7c329d8cb750a
dgecon <dgeco17@gmail.com>
2026-02-04 19:30:16 +0300

    add host

--- VanillaParket/VanillaParket/settings.py
+++ VanillaParket/VanillaParket/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a^2*x-pmlt5!4uw(3y&nd(ouz!+e2wz4qp-5i(33fhv97hj6g&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [185.184.123.212]
+ALLOWED_HOSTS = ['185.184.123.212','127.0.0.1']
 
 
 # Application definition
